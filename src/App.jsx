e289eb4aa13d289c1823af6871791abd055cce7b
amena2216100006 <amenaakter2216100006@gmail.com>
2025-04-18 01:30:29 +0600

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
 import Batsman from './Batsman'
+import Users from './Users'
+import Friends from './Friends'
+import Posts from './Posts'
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res=>res.json())
+
+// const fetchFriends = async() =>{
+//   const res = await fetch ("https://jsonplaceholder.typicode.com/users");
+//   return res.json();
+// }
+
+const fetchPosts = async() => {
+  const res = await fetch ('https://jsonplaceholder.typicode.com/posts');
+  return res.json();
+}
 
 function App() {
-
+  // const friendsPromise = fetchFriends();
+  const postsPromise = fetchPosts();
   function handleClick(){
     alert('I am clicked!')
   }
@@ -23,6 +40,18 @@ function App() {
       <h1>Vite + React</h1>
       <Batsman></Batsman>
       <Counter></Counter>
+
+      <Suspense fallback={<h2>Posts are coming......</h2>}>
+        <Posts postsPromise={postsPromise}></Posts>
+      </Suspense>
+
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+      <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      {/* <Suspense fallback={<h3>friends are coming for treat.....</h3>}>
+        <Friends friendsPromise= {friendsPromise}></Friends>
+      </Suspense> */}
       <button onClick={ ()=> handleAdd5(0)} >click to add 5</button>
       <button onClick={handleClick}>Click me</button>
       <button onClick={handleClick3}>Click me3</button>
